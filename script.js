b847e3e5a28f4f3863b6d87f7ef2ca9aca9a64db
unknown <khaledbadawy315@gmail.com>
2025-03-13 14:21:04 +0200

    addsearch

--- script.js
+++ script.js
@@ -42,32 +42,39 @@ function displayUsers() {
 
   let request = store.getAll();
   request.onsuccess = function () {
-    renderUserList(request.result);
+    let userList = document.getElementById("userList");
+    userList.innerHTML = "";
+
+    request.result.forEach((user) => {
+      let li = document.createElement("li");
+      li.innerHTML = `
+      <div>    ${user.name} </div>
+            <div> <button class="update-btn" data-id="${user.id}" data-name="${user.name}">Update</button>
+                <button class="delete-btn" data-id="${user.id}">delete</button></div>
+               
+            `;
+      userList.appendChild(li);
+    });
+
+    document.querySelectorAll(".delete-btn").forEach((button) => {
+      button.addEventListener("click", function (event) {
+        let userId = Number(event.target.getAttribute("data-id"));
+        deleteUser(userId);
+      });
+    });
+
+    document.querySelectorAll(".update-btn").forEach((button) => {
+      button.addEventListener("click", function (event) {
+        selectedUserId = Number(event.target.getAttribute("data-id"));
+        let userName = event.target.getAttribute("data-name");
+        document.getElementById("username").value = userName;
+        document.getElementById("updateBtn").style.display = "inline-block";
+      });
+    });
   };
 }
 
-function renderUserList(users) {
-  let searchValue = document.getElementById("search").value.toLowerCase();
-  let userList = document.getElementById("userList");
-  userList.innerHTML = "";
-
-  users.forEach((user) => {
-    if (user.name.toLowerCase().includes(searchValue)) {
-      let li = document.createElement("li");
-      li.innerHTML = `
-        <div>${user.name}</div>
-        <div>
-          <button class="update-btn" data-id="${user.id}" data-name="${user.name}">Update</button>
-          <button class="delete-btn" data-id="${user.id}">Delete</button>
-        </div>
-      `;
-      userList.appendChild(li);
-    }
-  });
-
-  addEventListeners();
-}
-
+// حذف مستخدم
 function deleteUser(id) {
   let transaction = db.transaction("users", "readwrite");
   let store = transaction.objectStore("users");
@@ -100,13 +107,31 @@ function updateUser() {
     };
   };
 }
-
 function searchUsers() {
+  let searchValue = document.getElementById("search").value.toLowerCase();
+
   let transaction = db.transaction("users", "readonly");
   let store = transaction.objectStore("users");
 
   let request = store.getAll();
   request.onsuccess = function () {
-    renderUserList(request.result);
+    let userList = document.getElementById("userList");
+    userList.innerHTML = "";
+
+    request.result.forEach((user) => {
+      if (user.name.toLowerCase().includes(searchValue)) {
+        let li = document.createElement("li");
+        li.innerHTML = `
+          <div>${user.name}</div>
+          <div>
+            <button class="update-btn" data-id="${user.id}" data-name="${user.name}">Update</button>
+            <button class="delete-btn" data-id="${user.id}">Delete</button>
+          </div>
+        `;
+        userList.appendChild(li);
+      }
+    });
+
+    addEventListeners();
   };
 }
